6d4ad1db422fad8b4574dbcbece0752c9e61f368
vasquezmoni27 <monica@Monicas-MacBook-Air.local>
2023-11-07 21:42:45 -0700

    Second Commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,3 +4,11 @@ import csv
 csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
 with open(csvpath) as csvfile
     csvreader = csv.reader(csvfile, delimiter=',')
+    
+    print(csvreader)
+
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    for row in csvreader
+        print(row)
